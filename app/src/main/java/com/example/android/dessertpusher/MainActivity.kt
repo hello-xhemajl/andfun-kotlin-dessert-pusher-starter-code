3f56531e38b5c475d67b03952fcd2d97f928f05d
xhemajl <xhemajl@post.com>
2020-04-28 17:29:49 +0200

    Start and stop timer onStart and onStop callbacks

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer : DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -83,6 +84,9 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
+
+        // Initialize the timer
+        dessertTimer = DessertTimer()
     }
 
     override fun onResume() {
@@ -98,11 +102,12 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
         Timber.i("onStart() called")
+        dessertTimer.startTimer()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Timber.i("onDesert() called")
+        Timber.i("onDestroy() called")
     }
 
     override fun onRestart() {
@@ -113,6 +118,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop() called")
+        dessertTimer.stopTimer()
     }
 
     /**
